c8e46f4f18c70061f1500269e8fd5a32629dd114
l23steve <steve@l23.ai>
2025-07-07 11:46:48 -0600

    Refine scan output structure

--- tests/utils/test_helpers.py
+++ tests/utils/test_helpers.py
@@ -1,5 +1,9 @@
-from lilac.utils.helpers import helper
+from lilac.utils.helpers import helper, sanitize_filename
 
 
 def test_helper() -> None:
     assert helper() is True
+
+
+def test_sanitize_filename() -> None:
+    assert sanitize_filename("my bucket/v1") == "my_bucket_v1"
